104ed30a4109245c3ce862c5e583988ea21f63b4
Voxelstice <95834462+Voxelstice@users.noreply.github.com>
2023-04-27 22:08:13 +0300

    Honestly i think i should just use Git instead

---
+++ src/classes/vector.js
@@ -0,0 +1,13 @@
+// Vector-helper vector3 only
+class Vec3 {
+    constructor(x, y, z) {
+        this.xCoord = x
+        this.yCoord = y
+        this.zCoord = z
+    }
+
+    rotateAroundY(amount) {
+        // Help needed
+        this.yCoord += amount
+    }
+}

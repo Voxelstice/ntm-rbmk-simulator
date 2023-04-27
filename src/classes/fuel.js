fbdc23a2a174c61bb839b4e1e5ad17b0d6dfda17
Voxelstice <95834462+Voxelstice@users.noreply.github.com>
2023-04-27 22:08:29 +0300

    Honestly i think i should just use Git instead

--- src/classes/fuel.js
+++ src/classes/fuel.js
@@ -4,6 +4,143 @@
         The existing fuel classes: https://github.com/HbmMods/Hbm-s-Nuclear-Tech-GIT/blob/master/src/main/java/com/hbm/items/ModItems.java#L3572
 */
 
+// Names (uses fullName because I set them while making the classes for them)
+const fuelNames = {
+    rbmk_fuel_empty: {
+        rod: "Empty RBMK Fuel Rod",
+        fullName: "It's empty. What do you expect?"
+    },
+    rbmk_fuel_test: {
+        rod: "rbmk_fuel_test",
+        fullName: "THE VOICES"
+    },
+    rbmk_fuel_ueu: {
+        rod: "NU RBMK Fuel Rod",
+        fullName: "Unenriched Uranium"
+    },
+    rbmk_fuel_meu: {
+        rod: "MEU RBMK Fuel Rod",
+        fullName: "Medium Enriched Uranium-235"
+    },
+    rbmk_fuel_heu233: {
+        rod: "HEU-233 RBMK Fuel Rod",
+        fullName: "Highly Enriched Uranium-233"
+    },
+    rbmk_fuel_heu235: {
+        rod: "HEU-235 RBMK Fuel Rod",
+        fullName: "Highly Enriched Uranium-235"
+    },
+    rbmk_fuel_thmeu: {
+        rod: "ThMEU RBMK Fuel Rod",
+        fullName: "Thorium with MEU Driver Fuel"
+    },
+    rbmk_fuel_lep: {
+        rod: "LEP-239 RBMK Fuel Rod",
+        fullName: "Low Enriched Plutonium-239"
+    },
+    rbmk_fuel_mep: {
+        rod: "MEP-239 RBMK Fuel Rod",
+        fullName: "Medium Enriched Plutonium-239"
+    },
+    rbmk_fuel_hep: {
+        rod: "HEP-239 RBMK Fuel Rod",
+        fullName: "Highly Enriched Plutonium-239"
+    },
+    rbmk_fuel_hep241: {
+        rod: "HEP-241 RBMK Fuel Rod",
+        fullName: "Highly Enriched Plutonium-241"
+    },
+    rbmk_fuel_lea: {
+        rod: "LEA RBMK Fuel Rod",
+        fullName: "Low Enriched Americium-242"
+    },
+    rbmk_fuel_mea: {
+        rod: "MEA RBMK Fuel Rod",
+        fullName: "Medium Enriched Americium-242"
+    },
+    rbmk_fuel_hea241: {
+        rod: "HEA-241 RBMK Fuel Rod",
+        fullName: "Highly Enriched Americium-241"
+    },
+    rbmk_fuel_hea242: {
+        rod: "HEA-242 RBMK Fuel Rod",
+        fullName: "Highly Enriched Americium-242"
+    },
+    rbmk_fuel_men: {
+        rod: "MEN RBMK Fuel Rod",
+        fullName: "Medium Enriched Neptunium-237"
+    },
+    rbmk_fuel_hen: {
+        rod: "HEN RBMK Fuel Rod",
+        fullName: "Highly Enriched Neptunium-237"
+    },
+    rbmk_fuel_mox: {
+        rod: "MOX RBMK Fuel Rod",
+        fullName: "Mixed MEU & LEP Oxide"
+    },
+    rbmk_fuel_les: {
+        rod: "LES RBMK Fuel Rod",
+        fullName: "Low Enriched Schrabidium-326"
+    },
+    rbmk_fuel_mes: {
+        rod: "MES RBMK Fuel Rod",
+        fullName: "Medium Enriched Schrabidium-326"
+    },
+    rbmk_fuel_hes: {
+        rod: "HES RBMK Fuel Rod",
+        fullName: "Highly Enriched Schrabidium-326"
+    },
+    rbmk_fuel_leaus: {
+        rod: "LEAus RBMK Fuel Rod",
+        fullName: "Low Enriched Australium (Tasmanite)"
+    },
+    rbmk_fuel_heaus: {
+        rod: "HEAus RBMK Fuel Rod",
+        fullName: "Highly Enriched Australium (Ayerite)"
+    },
+    rbmk_fuel_po210be: {
+        rod: "Po210Be RBMK Neutron Source",
+        fullName: "Polonium-210 & Beryllium Neutron Source"
+    },
+    rbmk_fuel_ra226be: {
+        rod: "Ra226Be RBMK Neutron Source",
+        fullName: "Radium-226 & Beryllium Neutron Source"
+    },
+    rbmk_fuel_pu238be: {
+        rod: "Pu238Be RBMK Neutron Source",
+        fullName: "Plutonium-238 & Beryllium Neutron Source"
+    },
+    rbmk_fuel_balefire_gold: {
+        rod: "Flashgold RBMK Fuel Rod",
+        fullName: "Antihydrogen in a Magnetized Gold-198 Lattice"
+    },
+    rbmk_fuel_flashlead: {
+        rod: "Flashlead RBMK Fuel Rod",
+        fullName: "Antihydrogen confined by a Magnetized Gold-198 and Lead-209 Lattice"
+    },
+    rbmk_fuel_balefire: {
+        rod: "Balefire RBMK Fuel Rod",
+        fullName: "Draconic Flames"
+    },
+    rbmk_fuel_zfb_bismuth: {
+        rod: "Bismuth RBMK ZFB Rod",
+        fullName: "Zirconium Fast Breeder - LEU/HEP-241#Bi"
+    },
+    rbmk_fuel_zfb_pu241: {
+        rod: "Pu-241 RBMK ZFB Rod",
+        fullName: "Zirconium Fast Breeder - HEU-235/HEP-240#Pu-241"
+    },
+    rbmk_fuel_zfb_am_mix: {
+        rod: "Fuel Grade Americium RBMK ZFB Rod",
+        fullName: "Zirconium Fast Breeder - HEP-241#MEA"
+    },
+    rbmk_fuel_drx: {
+        rod: `<span style="color: red;">Digamma RBMK Fuel Rod</span>`,
+        fullName: "can't you hear, can't you hear the thunder?"
+    },
+}
+
+// Enums (They would be actually enums if it was in TypeScript)
 const NType = {
     SLOW: "SLOW",
     FAST: "FAST",
@@ -39,6 +176,7 @@ const EnumDepleteFunc = {
     STATIC: "STATIC"
 }
 
+// Classes
 class RBMKFuel {
     constructor() {
         this.column = null
